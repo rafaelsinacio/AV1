2006f7886047a89ff4bc36b76d251c1c609f43f8
rafaelsinacio <rafael.-siqueira@hotmail.com>
2025-04-23 21:13:20 -0300

    AV1

---
+++ AV1/src/main/java/Rafael/Siqueira/AV1/Av1Application.java
@@ -0,0 +1,13 @@
+package Rafael.Siqueira.AV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Av1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Av1Application.class, args);
+	}
+
+}
---
+++ AV1/src/test/java/Rafael/Siqueira/AV1/Av1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Rafael.Siqueira.AV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Av1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
